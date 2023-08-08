25c5fbc82673d2bbcf07ecba166fe21c69a5fb8a
sarvocognizant <sarvottamsingh9798@gmail.com>
2023-08-08 20:43:18 +0530

    yaml change

--- src/main/java/managers/AllDriverManager.java
+++ src/main/java/managers/AllDriverManager.java
@@ -66,7 +66,7 @@ public class AllDriverManager {
     private WebDriver createRemoteDriver() throws MalformedURLException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--start-maximized");
-        webDriver = new RemoteWebDriver(new URL("http://34.102.33.64:4444/"), chromeOptions);
+        webDriver = new RemoteWebDriver(new URL(""), chromeOptions);
         long time = FileReaderManager.getInstance().getConfigFileReader().getTime();
         webDriver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
         return webDriver;
